f5ed867df4a4d201a52cc301bcbe350d85316ee7
ClaudioAnzini <claudioanzinineto123@gmail.com>
2024-12-14 19:43:05 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup></script>
+<script setup>
+import logo from '@/assets/img/logo.png'
+</script>
 
 <template>
   <header>
     <nav>
+      <div id="logo">
+        <img :src="logo" alt="Logo" />
+      </div>
       <router-link to="/">Home</router-link>
       <router-link to="/filmes">Filmes</router-link>
       <router-link to="/tv">Programas de TV</router-link>
+      <div class="search-container">
+        <input
+          v-model="searchQuery"
+          type="text"
+          placeholder="Pesquise aqui..."
+          @keyup.enter="handleSearch"
+          class="search-input"
+        />
+      </div>
     </nav>
   </header>
   <main>
@@ -15,7 +29,7 @@
 
 <style scoped>
 header {
-  height: 3rem;
+  height: 5rem;
   display: flex;
   background-color: black;
   color: #fff;
@@ -33,5 +47,17 @@ nav {
 nav a {
   text-decoration: none;
   color: #fff;
+}
+#app img {
+  display: flex;
+  align-items: center;
+  margin-right: 2rem;
+  width: 10rem;
+}
+.search-input {
+  padding: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+  width: 20rem;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,5 @@
+<script setup></script>
+
 <template>
   <h1>Home</h1>
   <p>Seja bem-vindo ao site de filmes e programas de TV!</p>
